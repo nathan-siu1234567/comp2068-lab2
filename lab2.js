9b8dfc3b77ad7c4f4441a223d4a36b0d55a8511c
nathan-siu <nathansiu9730@gmail.com>
2015-09-30 16:27:54 -0400

    i added lab2.js file

---
+++ lab2.js
@@ -0,0 +1,61 @@
+﻿var http = require('http');
+
+
+var url=require('url');
+
+http.createServer(function(req,res){
+res.writeHead(200, {'content-type': 'text-plain'});
+
+res.write('<h1>Simple Calculator</h1>')
+
+//get the 3 parameters from querystring
+var qs = url.parse(req.url, true).query;
+var method = qs.method;
+var x = parseFloat(qs.x);
+var y= parseFloat(qs.y);
+
+//validate operator and calculate the results
+
+var result =0;
+
+	if(method=="add")
+	{
+		result =x+y;
+		res.write(x + '+' + y + '=' + result);
+	}
+	else if(method=="subtract")
+	{
+		result =x-y;
+		res.write(x + '-' + y + '=' + result);
+
+	}
+	else if(method=="multiply")
+	{
+		result =x*y;
+		res.write(x + '*' + y + '=' + result);
+
+	}
+	else if(method=="divide")
+	{
+		result=x/y;
+		res.write(x + '/' + y + '=' + result);
+
+	}
+	else
+	{
+		res.write("please enter a valid method");
+	}
+	
+	return result;
+
+
+
+
+//display results to screen
+//res.write('sub-total: ' + operator + '<br />Tax: ' + x + '<br />Total: ' +y);
+
+res.end();
+
+}).listen(3000);
+
+
